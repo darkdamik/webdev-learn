6feecf19226e194af73856aad51ae43a02ceb62b
darkdamik <32012496+darkdamik@users.noreply.github.com>
2017-10-07 13:27:55 +0300

    create repository

---
+++ lw1/javascript/search-rnd-array-element.js
@@ -0,0 +1,31 @@
+﻿var c = 1;
+var poisk;
+var N = 9; 
+var arr = [];
+var isFound = false;
+var vivod = "";
+
+for (i = 0; i <= N; i++) { 
+  arr[i] = Math.floor(Math.random() * (16 - 1)) + 1;  
+  vivod = vivod + "Число " + c++ + ": " + arr[i] + "\n";
+}
+alert(vivod);
+
+var poisk = prompt("Число для поиска: "); 
+if ((poisk == "") || (isNaN(poisk)) || (poisk == null)) {
+  alert("Ошибка ввода");
+  windows.stop;
+}
+
+for (i = 0; i <= N; i++) {
+  if (arr[i] == poisk) {
+    isFound = true;
+  }
+}  	
+
+if (!isFound) { 
+  vivod = "Число: " + poisk + " не найдено";
+} else {
+  vivod = "Число: " + poisk + " найдено";
+}
+alert(vivod);
